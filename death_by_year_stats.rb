36faa66ff0b726ef6dce31fc5b963dbd81494502
fred <frederic.boisguerin@edulib.fr>
2020-03-31 09:05:04 +0200

    Refactor death_by_year_stats.rb

--- death_by_year_stats.rb
+++ death_by_year_stats.rb
@@ -3,25 +3,33 @@
 
 CARRIAGE_UNIX = "\n"
 POPULATION_DATABASE_PATH = 'population/database'
+YDAYS = (1..365).to_a.freeze
 
 def replace_CR_to_unix(string)
   string.gsub!(/\r\n?/, CARRIAGE_UNIX)
 end
 
-def load_populations
-  filenames = `ls #{POPULATION_DATABASE_PATH}`
+def files_in_dir(path)
+  filenames = `ls #{path}`
   replace_CR_to_unix filenames
+  filenames.split(CARRIAGE_UNIX)
+end
+
+def lines_in_file(*args)
+  lines = File.open(File.join(args)).read
+  replace_CR_to_unix lines
+  lines.split(CARRIAGE_UNIX)
+end
+
+def load_populations
   populations = Hash.new({})
-  filenames.split(CARRIAGE_UNIX).map do |filename|
+  files_in_dir(POPULATION_DATABASE_PATH).map do |filename|
     # puts "<#{filename.scan(/([^.]*).csv/)}>"
     year = filename[/\d+/]
-    lines = File.open(File.join(POPULATION_DATABASE_PATH, filename)).read
-    replace_CR_to_unix lines
-    populations[year] = lines.split(CARRIAGE_UNIX).map { |line| line.split(';') }.reduce(populations[year]) { |hash, line_split| hash.merge(line_split[0] => line_split[1]) }
+    populations[year] = lines_in_file(File.join(POPULATION_DATABASE_PATH, filename)).map { |line| line.split(';') }.reduce(populations[year]) { |hash, line_split| hash.merge(line_split[0] => line_split[1]) }
   end
   populations
 end
-
 
 
 puts load_populations
